c81af9c566dd13edabb3664cde2313a49a73cbe3
haseena-hassan <haseena2199@gmail.com>
2020-04-14 13:33:24 +0530

    reordering

--- vizmodel/src/App.js
+++ vizmodel/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import './Topbar/Topbar';
+import Topbar from './Topbar/Topbar';
 
 function App() {
   return (
-    
+    <div>
+      <Topbar/>
+    </div>
   );
 }
 
--- vizmodel/src/Topbar/Topbar.js
+++ vizmodel/src/Topbar/Topbar.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import './Topbar.css'
 
 
 function Topbar()   {
     return(
-
+        <div id="bar">
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-google"></a>
+            <a href="#" class="fa fa-linkedin"></a>
+            <a href="#" class="fa fa-youtube"></a>
+        </div>
     );
 }
+
+export default Topbar;
