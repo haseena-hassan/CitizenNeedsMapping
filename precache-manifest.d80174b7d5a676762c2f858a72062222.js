089f77c0fd3a5e8a1341d401480b41d69b7d7706
adarshsuresh07 <adarshsuresh@cet.ac.in>
2020-05-17 19:26:19 +0530

    Updates

---
+++ precache-manifest.d80174b7d5a676762c2f858a72062222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ccd7440e8c6ab6ddc31f6be8dd3627",
+    "url": "/Hassi/index.html"
+  },
+  {
+    "revision": "69f967348f28f8e7c6a6",
+    "url": "/Hassi/static/css/main.2a10f52d.chunk.css"
+  },
+  {
+    "revision": "77721382c937a6a17322",
+    "url": "/Hassi/static/js/2.9aafaa52.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Hassi/static/js/2.9aafaa52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f967348f28f8e7c6a6",
+    "url": "/Hassi/static/js/main.e7a45dbd.chunk.js"
+  },
+  {
+    "revision": "c2f8e652b1d38bf7ae28",
+    "url": "/Hassi/static/js/runtime-main.6d417f8a.js"
+  },
+  {
+    "revision": "2da0f1d54a3d5fb075577d742d10a3ad",
+    "url": "/Hassi/static/media/Logo.2da0f1d5.png"
+  }
+]);
